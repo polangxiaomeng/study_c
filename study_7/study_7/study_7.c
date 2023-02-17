121ddf83a02b13b98568c90c8cdf6b79aabecda4
polang-xiaomeng <1592272237@qq.com>
2023-02-17 18:01:04 +0800

    初阶指针

---
+++ study_7/study_7/study_7.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 10;//在内存中开辟一块空间
+//	int* p = &a;//这里我们对变量a，取出它的地址，可以使用&操作符。
+//	//a变量占用4个字节的空间，这里是将a的4个字节的第一个字节的地址存放在p变量中，p就是一个之指针变量。
+//	return 0;
+//}
