b3e20d5e0eebdcf1389980bb75a99d03b579a319
polang-xiaomeng <1592272237@qq.com>
2023-02-18 12:58:39 +0800

    初阶指针（超级详细）

--- study_7/study_7/study_7.c
+++ study_7/study_7/study_7.c
@@ -8,3 +8,185 @@
 //	//a变量占用4个字节的空间，这里是将a的4个字节的第一个字节的地址存放在p变量中，p就是一个之指针变量。
 //	return 0;
 //}
+
+
+//int num = 10;
+//p = &num;
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	int n = 10;
+//	char* pc = (char*)&n;
+//	int* pi = &n;
+//	printf("%p\n", &n);
+//	printf("%p\n", pc);
+//	printf("%p\n", pc + 1);
+//	printf("%p\n", pi);
+//	printf("%p\n", pi + 1);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int n = 0x11223344;
+//	char* pc = (char*)&n;
+//	int* pi = &n;
+//	*pc = 0; //重点在调试的过程中观察内存的变化。
+//	*pi = 0; //重点在调试的过程中观察内存的变化。
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int* p;//局部变量指针未初始化，默认为随机值
+//	*p = 20;
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int* p = arr;
+//	int i = 0;
+//	for (i = 0; i <= 11; i++)
+//	{
+//		//当指针指向的范围超出数组arr的范围时，p就是野指针
+//		*(p++) = i;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//char* test()
+//{
+//	char a[] = "hahaha";
+//	return a;
+//}
+//
+//int main()
+//{
+//	char* p = test();
+//	printf("%s", p);
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	int* p = NULL;
+//	int a = 10;
+//	p = &a;
+//	if (p != NULL)
+//	{
+//		*p = 20;
+//	}
+//	return 0;
+//}
+
+//#define N_VALUES 5
+//int main()
+//{
+//	float values[N_VALUES];
+//	float* vp;
+//	//指针+-整数；指针的关系运算
+//	for (vp = &values[0]; vp < &values[N_VALUES];)
+//	{
+//		*vp++ = 0;
+//	}
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int my_strlen(char* s)
+//{
+//	char* p = s;
+//	while (*p != '\0')
+//		p++;
+//	return p - s;
+//}
+//
+//int main()
+//{
+//	printf("%d", my_strlen("hello world"));
+//	return 0;
+//}
+
+
+//for (vp = &values[N_VALUES]; vp > &values[0];)
+//{
+//	*--vp = 0;
+//}
+
+
+//for (vp = &values[N_VALUES - 1]; vp >= &values[0]; vp--)
+//{
+//	*vp = 0;
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,0 };
+//	printf("%p\n", arr);
+//	printf("%p\n", &arr[0]);
+//	return 0;
+//}
+
+//int arr[10] = { 1,2,3,4,5,6,7,8,9,0 };
+//int* p = arr;//p存放的是数组首元素的地址
+
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,0 };
+//	int* p = arr; //指针存放数组首元素的地址
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (int i = 0; i < sz; i++)
+//	{
+//		printf("&arr[%d] = %p <====> p+%d = %p\n", i, &arr[i], i, p + i);
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+//	int* p = arr; //指针存放数组首元素的地址
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", *(p + i));
+//	}
+//	return 0;
+//}
+
+
+//int b = 20;
+//*ppa = &b;//等价于 pa = &b;
+
+
+//int b = 20;
+//*ppa = &b;//等价于 pa = &b;
+
+
+//** ppa = 30;
+////等价于*pa = 30;
+////等价于a = 30;
+//int arr1[5];
+//char arr2[6];
+
+
+
+//int arr1[5];
+//char arr2[6];
+int* arr3[5];//是什么？
